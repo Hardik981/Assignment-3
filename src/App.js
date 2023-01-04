bf865b116639c5437c317f456b6f5e798ff37792
Hardik981 <41016443+Hardik981@users.noreply.github.com>
2023-01-04 12:27:37 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from "react";
 import "./style.css";
 
 export default function App() {
+  let data = [{score:75, id:1},{score:54, id:2},{score:96, id:3},{score:82, id:4},{score:66, id:5}]
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+     {data.map((value)=><h1 key={value.id}>{value.score}</h1>)}
     </div>
   );
 }
